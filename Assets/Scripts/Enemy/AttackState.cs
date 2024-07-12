fb2ef8dd62f0557a8593cc14628176024a922fa7
KGr1sha <krylov.grigory@gmail.com>
2024-07-12 21:34:34 +0300

    enemy is skeleton

--- Assets/Scripts/Enemy/AttackState.cs
+++ Assets/Scripts/Enemy/AttackState.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Runtime.InteropServices;
 using BHSCamp.FSM;
 using UnityEngine;
 
@@ -9,19 +7,22 @@ namespace BHSCamp
     {
         private float _attackCD;
         private Enemy _enemy;
+        private Animator _animator;
 
         private float timer;
+        private bool _canExit;
 
-        public AttackState(Fsm fsm, Enemy enemy, float attackCD) : base(fsm)
+        public AttackState(Fsm fsm, Enemy enemy, float attackCD, Animator animator) : base(fsm)
         {
             _attackCD = attackCD;
             _enemy = enemy;
+            _animator = animator;
         }
 
         public override void Enter()
         {
-            Debug.Log("Attack state enter");
             timer = 0;
+            Attack();
         }
 
         public override void Update(float deltaTime)
@@ -29,8 +30,23 @@ namespace BHSCamp
             timer += deltaTime;
             if (timer >= _attackCD && IsPlayerInRange())
             {
+                Attack();
                 timer = 0;
             }
+
+            if (_canExit == false && timer >= 1f)
+                _canExit = true;
+
+            if (IsPlayerInRange() == false && _canExit)
+            {
+                Fsm.SetState<PatrolState>();
+            }
+        }
+
+        private void Attack()
+        {
+            _animator.SetTrigger("Attack");
+            _canExit = false;
         }
 
         private bool IsPlayerInRange()
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -15,15 +15,20 @@ namespace BHSCamp
         [Header("Attack")]
         [SerializeField] private float _attackCD = 1f;
 
+        private Animator _animator;
+        private Rigidbody2D _body;
+
         private Fsm _fsm;
         private IMove _move;
 
         private void Start()
         {
             _move = GetComponent<IMove>();
+            _animator = GetComponent<Animator>();
+            _body = GetComponent<Rigidbody2D>();
             _fsm = new Fsm();
             _fsm.AddState(new PatrolState(_fsm, this, _patrolSpeed, _waypoints, _move, transform));
-            _fsm.AddState(new AttackState(_fsm, this, _attackCD));
+            _fsm.AddState(new AttackState(_fsm, this, _attackCD, _animator));
             _fsm.SetState<PatrolState>();
         }
 
@@ -37,14 +42,18 @@ namespace BHSCamp
                 _boxCastSize.x / 2,
                 _playerLayerMask
             );
-            if (hit)
-                print(hit.collider.gameObject.name);
             return hit;
         }
 
         private void Update()
         {
             _fsm.Update(Time.deltaTime);
+            if (_body.velocity.x != 0)
+                transform.localScale = new Vector3(
+                    Mathf.Abs(transform.localScale.x) * Mathf.Sign(_body.velocity.x),
+                    transform.localScale.y,
+                    transform.localScale.z
+                );
         }
 
         private void FixedUpdate()
