f6913c073cf9a4d24931bbbf110ba65e713c5edc
KGr1sha <krylov.grigory@gmail.com>
2024-07-17 18:21:01 +0300

    power uuuuups

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,7 +21,9 @@ namespace BHSCamp
         public void AddScore(int amount)
         {
             if (amount < 0)
-                throw new ArgumentOutOfRangeException($"Amount should be positive!: {gameObject.name}");
+                throw new ArgumentOutOfRangeException(
+                    $"Amount should be positive!: {gameObject.name}"
+                );
             _score += amount;
             Debug.Log("Score: " + _score.ToString());
         }
