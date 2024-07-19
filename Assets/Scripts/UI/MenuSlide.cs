849cdc987aef7b3a55203f70b69cb484aa3e6fd5
KGr1sha <krylov.grigory@gmail.com>
2024-07-19 18:15:07 +0300

    sliding menu

---
+++ Assets/Scripts/UI/MenuSlide.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace BHSCamp.UI
+{
+    public class MenuSlide : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _slideBegin;
+        [SerializeField] private RectTransform _slideEnd;
+        [SerializeField] private AnimationCurve _curve;
+        [SerializeField] private float _slideTime;
+        private RectTransform _rectTransform;
+        private float _time;
+        private bool _move;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Start()
+        {
+            Slide();
+        }
+        
+        public void Slide()
+        {
+            _rectTransform.position = _slideBegin.position;
+            _move = true;
+        }
+
+        private void Update()
+        {
+            if (false == _move) return;
+            _time += Time.deltaTime;
+            float t = Mathf.Clamp01(_time / _slideTime);
+            _rectTransform.position = Vector3.Lerp(
+                _slideBegin.position,
+                _slideEnd.position,
+               _curve.Evaluate((t))
+            );
+            if (Vector3.Distance(_rectTransform.position, _slideEnd.position) <= 0.1f)
+                _move = false;
+        }
+    }
+}
